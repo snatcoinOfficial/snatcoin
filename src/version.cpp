02fec2c59c6762fc788a6d9bd749febbdecf004e
Snatcoin <snatcoinOfficial@users.noreply.github.com>
2018-02-10 08:47:45 +1000

    Add files via upload

--- src/version.cpp
+++ src/version.cpp
@@ -8,7 +8,7 @@
 // Name of client reported in the 'version' message. Report the same name
 // for both bitcoind and bitcoin-qt, to make it harder for attackers to
 // target servers or GUI users specifically.
-const std::string CLIENT_NAME("Satoshi");
+const std::string CLIENT_NAME("Snatcoin Core Client");
 
 // Client version number
 #define CLIENT_VERSION_SUFFIX   "-beta"
@@ -36,8 +36,8 @@ const std::string CLIENT_NAME("Satoshi");
 // git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#    define GIT_COMMIT_ID "c1e46ab3a90b"
-#    define GIT_COMMIT_DATE "Mon, 15 Sep 2014 23:53:37 -1000"
+#    define GIT_COMMIT_ID "SNAT"
+#    define GIT_COMMIT_DATE "Sun, 11 Feb 2018 10:42:27 +1000"
 #endif
 
 #define BUILD_DESC_FROM_COMMIT(maj,min,rev,build,commit) \
